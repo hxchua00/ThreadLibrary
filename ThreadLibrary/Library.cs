1ec88b99ff5106740294257b44de6dc4f70cfe88
hxchua00 <hxchua00@gmail.com>
2021-10-06 02:34:30 +0800

    Add project files.

---
+++ ThreadLibrary/Library.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadLibrary
+{
+    class Student
+    {
+        public int studentID;
+        public string studentName;
+        public bool book;
+
+        public Student(int studentID, string studentName, bool book)
+        {
+            this.studentID = studentID;
+            this.studentName = studentName;
+            this.book = book;
+        }
+    }
+    class Library
+    {
+        static List<Student> Classroom = new List<Student>();
+        bool IssuedBook = false;
+
+        static Library()
+        {
+            Classroom.Add(new Student(1, "John", false));
+            Classroom.Add(new Student(2, "Mary", false));
+            Classroom.Add(new Student(3, "Owen", false));
+            Classroom.Add(new Student(4, "Harry", false));
+            Classroom.Add(new Student(5, "Ronald", false));
+            Classroom.Add(new Student(6, "Tatsuya", false));
+            Classroom.Add(new Student(7, "Kaguya", false));
+            Classroom.Add(new Student(8, "Moris", false));
+            Classroom.Add(new Student(9, "Eldo", false));
+            Classroom.Add(new Student(10, "Freya", false));
+        }
+
+        public bool VerifyStudent(int ID)
+        {
+            foreach(Student s in Classroom)
+            {
+                if(s.studentID == ID)
+                {
+                   
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool IsBookIssued()
+        {
+            if (!IssuedBook)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void BorrowBook(int ID)
+        {
+            bool checkbook = IsBookIssued();
+            if(checkbook == false)
+            {
+                foreach (Student s in Classroom)
+                {
+                    if(s.studentID == ID)
+                    {
+                        if(s.book == false)
+                        {
+                            s.book = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("There's some trouble, you cannot borrow the book now. Sorry!");
+                            Console.WriteLine();
+                            break;
+                        }
+                    }
+                }
+                Console.WriteLine("The book has been borrowed by: ");
+                PrintStudentDetails(ID);
+                IssuedBook = true;
+            }
+            else
+            {
+                Console.WriteLine("Sorry! It seems someone else has already borrowed the book!");
+                Console.WriteLine();
+            }
+        }
+
+        public void ReturnBook(int ID)
+        {
+            if (IsBookIssued() == true)
+            {
+                foreach (Student s in Classroom)
+                {
+                    if (s.studentID == ID)
+                    {
+                        if(s.book == true)
+                        {
+                            s.book = false;
+                            
+                        }
+                        else
+                        {
+                            Console.WriteLine("Is there a mistake? You don't have any book to return!");
+                            Console.WriteLine();
+                            break;
+                        }
+                    }
+                }
+                Console.WriteLine("Book is returned! Thank you!");
+                Console.WriteLine();
+                IssuedBook = false;
+            }
+            else
+            {
+                Console.WriteLine("Sorry! It seems someone else has already borrowed the book!");
+                Console.WriteLine();
+            }
+        }
+
+
+        public void PrintStudentDetails(int ID)
+        {
+            foreach(Student s in Classroom)
+            {
+                if(s.studentID == ID)
+                {
+                    Console.WriteLine("Student ID: {0}",s.studentID);
+                    Console.WriteLine("Student name: {0}",s.studentName);
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ ThreadLibrary/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread entrance = new Thread(()=> { LibEntry(); });
+            //entrance.IsBackground = true;
+            entrance.Start();
+        }
+
+        public static void LibEntry()
+        {
+            Library lib = new Library();
+
+            Console.WriteLine("Enter student ID: ");
+            int ID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            bool verification = lib.VerifyStudent(ID);
+            Thread InLib = new Thread(() => LibMenu(ID));
+            if (verification == true)
+            {
+                Console.WriteLine("Welcome to the library!");
+                lib.PrintStudentDetails(ID);
+                if (!InLib.IsAlive)
+                    InLib.Start();
+            }
+            else
+            {
+                Console.WriteLine("Student ID not found, access denied!");
+                Console.WriteLine();
+                Console.ReadLine();
+            }
+
+        }
+
+        public static void LibMenu(int ID)
+        {
+            Library lib = new Library();
+            bool cont = true;
+            while (cont)
+            {
+                Console.WriteLine("What do you wish to do at the Library?");
+                Console.WriteLine();
+                Console.WriteLine("1) Borrow book");
+                Console.WriteLine("2) ReturnBook");
+                Console.WriteLine("3) Nothing");
+                Console.WriteLine();
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        lib.BorrowBook(ID);
+                        break;
+                    case 2:
+                        lib.ReturnBook(ID);
+                        break;
+                    case 3:
+                        Console.WriteLine("I hope you found what you need! Goodbye!");
+                        Console.WriteLine();
+                        cont = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid procedure! Please behave in the Library!");
+                        break;
+                }
+            }
+        }
+    }
+}
